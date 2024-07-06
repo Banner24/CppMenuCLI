c9f9170795c30bcd5faecfd8fe157b9b9b95b7d0
Banner24 <hanfkeks24@live.de>
2024-07-06 21:13:09 +0200

    Add files via upload

--- Menu/Items/DemoItem1.h
+++ Menu/Items/DemoItem1.h
@@ -1,1 +1,36 @@
+#pragma once
 
+#include "../IMenuItem.h"
+#include "../MyMenu.h"
+/**
+ * @brief DemoItem1 class representing a specific menu item example.
+ * 
+ * This class demonstrates a specific implementation of an IMenuItem,
+ * providing functionality for setting its name and opening it,
+ * as well as interacting with a MyMenu instance.
+ */
+class DemoItem1 : public IMenuItem {
+public:
+    /**
+     * @brief Constructor for DemoItem1.
+     * 
+     * @param itemName The name of the menu item.
+     */
+    DemoItem1(std::string itemName) {
+        this->setItemName(itemName);
+    }
+
+    /**
+     * @brief Opens the menu item.
+     * 
+     * Overrides the Open method from IMenuItem.
+     * Prints a message indicating the selected item and starts a new MyMenu instance.
+     */
+    void Open() override {
+        printf("\nThis is Item: %s\n", this->getItemName().c_str());
+
+        // Example: Create a new MyMenu instance and start it
+        MyMenu *m = new MyMenu("Test Menu");
+        m->Start();
+    }
+};
---
+++ Menu/Items/DemoItem2.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "../IMenuItem.h"
+#include "../MyMenu.h"
+
+/**
+ * @brief DemoItem2 class representing another specific menu item example.
+ * 
+ * This class demonstrates another implementation of an IMenuItem,
+ * providing functionality for setting its name and opening it.
+ */
+class DemoItem2 : public IMenuItem {
+	public:
+		/**
+		 * @brief Constructor for DemoItem2.
+		 * 
+		 * @param itemName The name of the menu item.
+		 */
+		DemoItem2(std::string itemName) {
+			this->setItemName(itemName);
+		}
+
+		/**
+		 * @brief Opens the menu item.
+		 * 
+		 * Overrides the Open method from IMenuItem.
+		 * Prints a message indicating the selected item and waits for user input.
+		 * Here would be the point to add your code
+		 */
+		void Open() override {
+			printf("\nThis is Item: %s\n", this->getItemName().c_str());
+			
+			// Continue here!
+
+			// Example: Wait for user input, then get back to the previous menu 
+			getch();
+		}
+};
