dbe854ba77a69bbf60ed6aeab8af8866cbb9884f
Banner24 <hanfkeks24@live.de>
2024-07-06 21:27:43 +0200

    Update DemoItem1.h

--- Menu/Items/DemoItem1.h
+++ Menu/Items/DemoItem1.h
@@ -10,27 +10,27 @@
  * as well as interacting with a MyMenu instance.
  */
 class DemoItem1 : public IMenuItem {
-public:
-    /**
-     * @brief Constructor for DemoItem1.
-     * 
-     * @param itemName The name of the menu item.
-     */
-    DemoItem1(std::string itemName) {
-        this->setItemName(itemName);
-    }
+	public:
+		/**
+		 * @brief Constructor for DemoItem1.
+		 * 
+		 * @param itemName The name of the menu item.
+		 */
+		DemoItem1(std::string itemName) {
+			this->setItemName(itemName);
+		}
 
-    /**
-     * @brief Opens the menu item.
-     * 
-     * Overrides the Open method from IMenuItem.
-     * Prints a message indicating the selected item and starts a new MyMenu instance.
-     */
-    void Open() override {
-        printf("\nThis is Item: %s\n", this->getItemName().c_str());
+		/**
+		 * @brief Opens the menu item.
+		 * 
+		 * Overrides the Open method from IMenuItem.
+		 * Prints a message indicating the selected item and starts a new MyMenu instance.
+		 */
+		void Open() override {
+			printf("\nThis is Item: %s\n", this->getItemName().c_str());
 
-        // Example: Create a new MyMenu instance and start it
-        MyMenu *m = new MyMenu("Test Menu");
-        m->Start();
-    }
+			// Example: Create a new MyMenu instance and start it
+			MyMenu *m = new MyMenu("Test Menu");
+			m->Start();
+		}
 };
